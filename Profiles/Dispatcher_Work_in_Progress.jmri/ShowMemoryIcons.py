7959fc95c127936f00bae3e12e7d3bcdc40052ec
Bob Jacobsen <jacobsen@mac.com>
2025-06-02 06:47:42 -0400

    two scripts for hide/show memory icon borders

---
+++ Profiles/Dispatcher_Work_in_Progress.jmri/ShowMemoryIcons.py
@@ -0,0 +1,17 @@
+# ShowMemoryIcons.py
+#
+# This shows all the memory icons on the layout editor panel(s)
+# by setting their border visible
+# It's used to simplify a panel by showing the memory icon boxes on command
+#
+# Bob Jacobsen  2025
+
+import java
+import javax
+import jmri
+
+panelMenu = jmri.InstanceManager.getDefault(jmri.jmrit.display.EditorManager)
+layoutPanels = panelMenu.getList(jmri.jmrit.display.layoutEditor.LayoutEditor)
+for layoutEditor in layoutPanels :
+    for memoryIcon in layoutEditor.getMemoryLabelList() :
+        memoryIcon.setBorder(javax.swing.BorderFactory.createLineBorder(java.awt.Color.yellow))
