70f1d9da77c3043e8105cb2de6cd52cdb8363082
Bob Jacobsen <jacobsen@mac.com>
2025-11-04 15:31:00 -0500

    Add new Dispatcher Latest profile from D WIP

---
+++ Profiles/Dispatcher_Latest.jmri/HideMemoryIcons.py
@@ -0,0 +1,22 @@
+# HideMemoryIcons.py
+#
+# This hides all the memory icons on the layout editor panel(s)
+# by removing their border
+# It's used to simplify a panel by removing the memory icon boxes
+#
+# Bob Jacobsen  2025
+
+import java
+import javax
+import jmri
+
+panelMenu = jmri.InstanceManager.getDefault(jmri.jmrit.display.EditorManager)
+layoutPanels = panelMenu.getList(jmri.jmrit.display.layoutEditor.LayoutEditor)
+for layoutEditor in layoutPanels :
+    for memoryIcon in layoutEditor.getMemoryLabelList() :
+        memoryIcon.setBorder(javax.swing.BorderFactory.createEmptyBorder())
+        popupUtil = memoryIcon.getPopupUtility()
+        popupUtil.setBorderSize(0)
+        popupUtil.setBorderColor(layoutEditor.getBackgroundColor()) # makes it invisible
+        popupUtil.setBorder(False)
+    layoutEditor.repaint()
---
+++ Profiles/Dispatcher_Latest.jmri/HighlightUnknownBlockSensors.py
@@ -0,0 +1,56 @@
+# a script for the PMRRM to convert change the color on LayoutBlocks whose
+# sensor is in the Unknown state.  Only checks when run, i.e. at startup.
+#
+# Bob Jacobsen    2025
+
+import java
+import jmri
+from javax.swing import JOptionPane
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class ResetBlockColorListener(java.beans.PropertyChangeListener):
+  def set(self, sensor, block) :
+    self.sensor = sensor
+    self.block = block
+    self.savedColor = block.getBlockTrackColor()
+    return
+  def propertyChange(self, event):
+    if self.sensor.getKnownState() == UNKNOWN :
+        self.block.setBlockTrackColor(java.awt.Color(190, 190, 255))
+    else :
+        self.block.setBlockTrackColor(self.savedColor)
+        self.sensor.removePropertyChangeListener(self)
+    return
+
+class HighlightUnknownBlockSensors(jmri.jmrit.automat.AbstractAutomaton) :
+        
+    def handle(self):
+        self.log = org.slf4j.LoggerFactory.getLogger(
+            "jmri.jmrit.jython.exec.script.HighlightUnknownBlockSensors"
+        )
+        self.waitMsec(8000)  # compare to QueryLnSensorState
+        
+        self.log.info("Start HighlightUnknownBlockSensors")
+        
+        blockManager = jmri.InstanceManager.getDefault(jmri.jmrit.display.layoutEditor.LayoutBlockManager)
+        foundSome = False
+        
+        for sensor in sensors.getNamedBeanSet():
+            if sensor.getKnownState() == UNKNOWN :
+                block = blockManager.getBlockWithSensorAssigned(sensor)
+                if block != None :
+                    # have to deal with this one
+                    foundSome = True
+                    self.log.warn("Found sensor in UNKNOWN state:", sensor, "for layout block:", block)
+                    listener = ResetBlockColorListener()
+                    listener.set(sensor, block)
+                    sensor.addPropertyChangeListener(listener)
+                    listener.propertyChange(None)
+                    
+        # if foundSome :
+        #    JOptionPane.showMessageDialog(None,"Light blue lines are occupancy sensors that didn't report status","Some sensor states unknown",JOptionPane.INFORMATION_MESSAGE)
+            
+a = HighlightUnknownBlockSensors()
+a.setName("Highlight unknown block sensors")
+a.start()    
