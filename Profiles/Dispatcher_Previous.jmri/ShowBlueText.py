8a0656652068ed3ec3f1efb6ec8830aa7328a5c9
Bob Jacobsen <jacobsen@mac.com>
2025-11-04 15:31:40 -0500

    Add new Dispatcher Previous profile from D WIP

---
+++ Profiles/Dispatcher_Previous.jmri/ShowBlueText.py
@@ -0,0 +1,19 @@
+# ShowBlueText.py
+#
+# This shows all the blue content on the layout editor panel(s)
+# It's used to (re-)display the blue turnout numbers.
+#
+# Bob Jacobsen  2025
+
+import java
+import jmri
+
+panelMenu = jmri.InstanceManager.getDefault(jmri.jmrit.display.EditorManager)
+layoutPanels = panelMenu.getList(jmri.jmrit.display.layoutEditor.LayoutEditor)
+colorMatch = java.awt.Color(0,0,255)
+for layoutEditor in layoutPanels :
+    for positionable in layoutEditor.getContents() :
+        if positionable.getForeground() == colorMatch :
+            positionable.setVisible(True)
+    layoutEditor.repaint()
+    
