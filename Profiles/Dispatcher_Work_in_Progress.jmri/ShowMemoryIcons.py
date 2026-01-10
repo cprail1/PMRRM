e86e7edc96ad1dd40c26e1e42542b0667ffdfcbb
Bob Jacobsen <jacobsen@mac.com>
2026-01-10 17:23:38 -0500

    don't show boxes on optional blue label memories

--- Profiles/Dispatcher_Work_in_Progress.jmri/ShowMemoryIcons.py
+++ Profiles/Dispatcher_Work_in_Progress.jmri/ShowMemoryIcons.py
@@ -14,9 +14,10 @@ panelMenu = jmri.InstanceManager.getDefault(jmri.jmrit.display.EditorManager)
 layoutPanels = panelMenu.getList(jmri.jmrit.display.layoutEditor.LayoutEditor)
 for layoutEditor in layoutPanels :
     for memoryIcon in layoutEditor.getMemoryLabelList() :
-        popupUtil = memoryIcon.getPopupUtility()
-        popupUtil.setBorderColor(java.awt.Color.yellow)
-        popupUtil.setBorderSize(1)
-        popupUtil.setBorder(True)
-        memoryIcon.setBorder(javax.swing.BorderFactory.createLineBorder(java.awt.Color.yellow))
+        if memoryIcon.getMemory() != None and not memoryIcon.getMemory().getSystemName().startswith("IMLABEL$"):
+            popupUtil = memoryIcon.getPopupUtility()
+            popupUtil.setBorderColor(java.awt.Color.yellow)
+            popupUtil.setBorderSize(1)
+            popupUtil.setBorder(True)
+            memoryIcon.setBorder(javax.swing.BorderFactory.createLineBorder(java.awt.Color.yellow))
     layoutEditor.repaint()
