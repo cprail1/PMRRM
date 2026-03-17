70d111368a747d14245b0eaad8d2cd8a0dd30f75
Bob Jacobsen <jacobsen@mac.com>
2026-03-17 14:28:09 -0400

    utility script used for entering signal info

---
+++ UtilityScripts/PanelMastsToEventNames.py
@@ -0,0 +1,46 @@
+import xml.dom.minidom
+
+
+def getChildElementContent(element, name):
+    childElement = element.getElementsByTagName(name)[0]
+    return "".join([node.data for node in childElement.childNodes])
+
+def processAspect(username, aspect) :
+    aspectName = aspect.getAttribute('defines')
+    
+    if "Advanced" in aspectName : return  # in all disabled aspects on masts
+
+    aspectName = aspectName.replace("Approach", "App")
+    aspectName = aspectName.replace("Diverging", "Div")
+    aspectName = aspectName.replace("Secondary", "2nd")
+    aspectName = aspectName.replace(' ', '')
+
+    eventID = getChildElementContent(aspect, "event")
+    writeNameElement(username, aspectName, eventID)
+    
+def writeNameElement(username, aspectName, eventID) :
+    print(f"<entry><eventID>{eventID}</eventID><name>{username}:{aspectName}</name></entry>")
+    
+# 1. Parse the XML file into a Document object
+dom_tree = xml.dom.minidom.parse("DispatcherDefault.xml")
+# Get the root element
+root_element = dom_tree.documentElement
+
+# 2. Access elements by tag name
+masts = root_element.getElementsByTagName("olcbsignalmast")
+
+print ('<?xml version="1.0" encoding="UTF-8"?>')
+print ("<eventNameStore><names>")
+
+# 3. Iterate through elements and access their data
+for mast in masts:
+    username = getChildElementContent(mast, "userName")
+
+    aspects = mast.getElementsByTagName("aspect")
+    for aspect in aspects:
+        processAspect(username, aspect)
+
+print ("</names></eventNameStore>")
+
+# 4. Clean up the DOM tree (optional, but recommended for older Python versions)
+dom_tree.unlink()
