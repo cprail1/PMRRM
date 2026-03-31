a576bb430b73043274fa57d930e7abba6b06658f
Bob Jacobsen <jacobsen@mac.com>
2026-03-31 07:21:01 -0500

    panel order; hide Midway Freight panel at startup

--- Profiles/Dispatcher_Work_in_Progress.jmri/HideHiResPanel.py
+++ Profiles/Dispatcher_Work_in_Progress.jmri/HideHiResPanel.py
@@ -9,7 +9,7 @@ class HideHiResPanel(jmri.jmrit.automat.AbstractAutomaton) :
         self.waitMsec(2000)
 
         thisUser = java.lang.System.getProperty("user.name").lower()
-        # thisUser = "dispatch"  # here for debugging, comment out for normal operation
+        #thisUser = "dispatch"  # here for debugging, comment out for normal operation
         desiredUser = "dispatch"
         if thisUser != desiredUser:
             self.log.info("Skip hiding HiRes panel because user '{}' is not '{}'", thisUser, desiredUser)
@@ -23,6 +23,10 @@ class HideHiResPanel(jmri.jmrit.automat.AbstractAutomaton) :
         frame = jmri.util.JmriJFrame.getFrame("Port Area")
         frame.setVisible(False)         
         self.log.info("Set the Port Area panel invisible")
+
+        frame = jmri.util.JmriJFrame.getFrame("Midway Freight")
+        frame.setVisible(False)         
+        self.log.info("Set the Midway Freight panel invisible")
 
 HideHiResPanel().start()
         
