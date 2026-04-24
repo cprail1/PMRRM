2b024998efaf991e920fd46d372a02c33443c4ff
Bob Jacobsen <jacobsen@mac.com>
2026-04-24 07:48:37 -0400

    Clean up  HideHiResPanel.py

--- Profiles/Dispatcher_Work_in_Progress.jmri/HideHiResPanel.py
+++ Profiles/Dispatcher_Work_in_Progress.jmri/HideHiResPanel.py
@@ -12,33 +12,27 @@ class HideHiResPanel(jmri.jmrit.automat.AbstractAutomaton) :
         self.waitMsec(4000)
 
         thisUser = java.lang.System.getProperty("user.name").lower()
-        #thisUser = "dispatch"  # here for debugging, comment out for normal operation
+        # thisUser = "dispatch"  # here for debugging, comment out for normal operation
         desiredUser = "dispatch"
         if thisUser != desiredUser:
-            self.log.info("Skip hiding HiRes panel because user '{}' is not '{}'", thisUser, desiredUser)
+            self.log.info("Skip hiding panels because user '{}' is not '{}'", thisUser, desiredUser)
             return False # done early
             
-        # Now proceed to the proper windows
+        # Now proceed to windows that are to be hidden, but left in Windows menu
         frame = jmri.util.JmriJFrame.getFrame("PMRRM Dispatcher HiRes")
         frame.setVisible(False)         
         self.log.info("Set the HiRes panel invisible")
 
-        frame = jmri.util.JmriJFrame.getFrame("Port Area")
-        frame.setVisible(False)         
-        self.log.info("Set the Port Area panel hidden")
-
-        frame = jmri.util.JmriJFrame.getFrame("Midway Freight")
-        frame.setVisible(False)         
-        self.log.info("Set the Midway Freight panel hidden")
-        
-        frame = jmri.util.JmriJFrame.getFrame("Midway Engine Service")
-        frame.setVisible(False)         
-        self.log.info("Set the Midway Engine Service panel hidden")
-        
         # and set their Window menu entries disabled
         try :
             targets = ["Port Area", "Midway Freight", "Midway Engine Service"]
             jmri.util.WindowMenu.setIgnoredFrames(targets)
+            for target in targets :
+                self.log.info("Set the {} panel hidden", target)
+                frame = jmri.util.JmriJFrame.getFrame(target)
+                if frame is not None : frame.setVisible(False) 
+                else : self.log.error("Frame {} not found", target)        
+                
         except AttributeError:
             self.log.error("Could not adjust JMRI Window menu, you need at least JMRI 5.15.6")
 
