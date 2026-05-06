5dfcb3da2647834ce552bc9bc31f539792ebf05c
Bob Jacobsen <jacobsen@mac.com>
2026-05-06 18:52:43 -0400

    New GatewayPongGame script

---
+++ Profiles/Dispatcher_Work_in_Progress.jmri/GatewayPongGame.py
@@ -0,0 +1,60 @@
+# Check a LocoNet-LCC gateway by periodically 
+# sending a turnout command in each direction and
+# checking for receipt.
+#
+# Written by Bob Jacobsen for the PMRRM 2026
+#
+
+import java
+import jmri
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+lt = turnouts.provideTurnout("LT2021")
+mt = turnouts.provideTurnout("MTT2021")
+
+closedSound = jmri.jmrit.Sound("resources/sounds/Bell.wav")
+thrownSound = jmri.jmrit.Sound("resources/sounds/bell_stroke.wav")
+failSound = jmri.jmrit.Sound(jmri.util.FileUtil.getExternalFilename("profile:resources/sounds/EndGame.wav"))
+
+log = org.slf4j.LoggerFactory.getLogger(
+        "script.GatewayPongGame"
+        )
+
+class GatewayPongGame (jmri.jmrit.automat.AbstractAutomaton) :
+        
+    def handle(self) : 
+        delay = 3000   # in milliseconds
+        
+        mt.setCommandedState(CLOSED)
+        self.waitMsec(200)
+        if lt.getKnownState() == CLOSED :
+            # OK, play sound and continue
+            closedSound.play()
+        else : 
+            # failed, sound and quit
+            log.error("LCC -> LocoNet failed")            
+            failSound.play()
+            return False
+            
+        self.waitMsec(delay)
+        
+        lt.setCommandedState(THROWN)
+        self.waitMsec(200)
+        if mt.getKnownState() == THROWN :
+            # OK, play sound and continue
+            thrownSound.play()
+        else : 
+            # failed, sound and quit
+            log.error("LocoNet -> LCC failed")
+            failSound.play()
+            return False
+            
+        self.waitMsec(delay)
+        
+        # repeat
+        return True
+        
+t = GatewayPongGame()
+t.setName("GatewayPongGame")
+t.start()
